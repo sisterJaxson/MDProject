1f1639016d6564cc551eb25548845b403f1b7156
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-04-25 13:39:54 -0400

    Methods set up

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,6 +3,26 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
+    System.out.println("Monday's child is fair of face, ");
+    System.out.println("Tuesday's child is full of grace, ");
+    System.out.println("Wednesday's child is full of woe, ");
+    System.out.println("Thursday's child is far to go. ");
+    System.out.println("Friday's chile is loving and giving, ");
+    System.out.println("Saturday's child works hard for a living, ");
+    System.out.println("But the child born on the Sabbath Day, ");
+    System.out.println("Is fair and good and wise in every way.");
+
+    daysAndMonth();
+    dayOfTheWeek();
+    poemLine();
+    }
+    static void daysAndMonth(){
+
+    }
+    static void dayOfTheWeek(){
+
+    }
+    static void poemLine(){
 
     }
 }
