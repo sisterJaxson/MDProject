fd1a678056a142ab1bf1ec626ea87f0664f3de00
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-04-25 14:07:20 -0400

    Got starter questions done

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,5 +1,7 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,6 +13,16 @@ public class Main {
     System.out.println("Saturday's child works hard for a living, ");
     System.out.println("But the child born on the Sabbath Day, ");
     System.out.println("Is fair and good and wise in every way.");
+
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("What month were you born in? (number)");
+    keyboard.nextLine();
+    System.out.println("What day were you born on?");
+    keyboard.nextLine();
+    System.out.println("What year were you born in?");
+    keyboard.nextLine();
+
 
     daysAndMonth();
     dayOfTheWeek();
